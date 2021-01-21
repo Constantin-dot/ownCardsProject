e6e59e5db9f1ac30e9577083d91be62f44630e6b
Constantin-dot <konstantinkspb@gmail.com>
2021-01-21 17:25:04 +0300

    Updates

---
+++ precache-manifest.caf7f5faa86e53bdeddce34ca41fb08c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6619f0c6a434160900c325f8ed762c70",
+    "url": "/ownCardsProject/index.html"
+  },
+  {
+    "revision": "4c7abc4bae45a7d3e6cf",
+    "url": "/ownCardsProject/static/css/main.34e55649.chunk.css"
+  },
+  {
+    "revision": "f6ec1f2f402eebdbf9d2",
+    "url": "/ownCardsProject/static/js/2.6e1db14b.chunk.js"
+  },
+  {
+    "revision": "fea17e90e7ccac1a017279c0975d8084",
+    "url": "/ownCardsProject/static/js/2.6e1db14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7abc4bae45a7d3e6cf",
+    "url": "/ownCardsProject/static/js/main.4652435f.chunk.js"
+  },
+  {
+    "revision": "4ccb9bc04c79c0e144ea",
+    "url": "/ownCardsProject/static/js/runtime-main.2febfd01.js"
+  },
+  {
+    "revision": "5812c2c9ff9569e2f71116b3ec7c8711",
+    "url": "/ownCardsProject/static/media/Fitness_Fill.5812c2c9.svg"
+  },
+  {
+    "revision": "bbf08363d70e34eb61408db483f12eb6",
+    "url": "/ownCardsProject/static/media/Folder-Outline.bbf08363.svg"
+  },
+  {
+    "revision": "042251b44ba5725638fa1f8d74109219",
+    "url": "/ownCardsProject/static/media/Pencil-Outline.042251b4.svg"
+  },
+  {
+    "revision": "d76ce2371251c1908bf7c0da9180631d",
+    "url": "/ownCardsProject/static/media/Trash-Outline.d76ce237.svg"
+  }
+]);
